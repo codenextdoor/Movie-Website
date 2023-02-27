6ce9fd31cdb131fa02d04ecf3bf840a0cdcf852b
codenextdoor <xuluv911@gmail.com>
2023-02-27 11:48:03 +0545

    slider completed

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ const arrows = document.querySelectorAll(".arrow");
 const movieLists = document.querySelectorAll(".movie-list");
 
 arrows.forEach((arrow, i) => {
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  let clickCounter = 0;
   arrow.addEventListener("click", () => {
-    movieLists[i].style.transform = "translateX(-100px)";
+    clickCounter++;
+    if (itemNumber - (4 + clickCounter) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
   });
-  console.log(movieLists[i].computedStyleMap().get("transform")[0].x.value);
 });
